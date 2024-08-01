8703b7ae57c5236894fdce71d913c59d10e4ad9f
1337haxorguy <1337haxorguy1@gmail.com>
2024-08-01 15:34:10 -0400

    secrets

--- src/firebase.js
+++ src/firebase.js
@@ -15,12 +15,6 @@ const firebaseConfig = {
   messagingSenderId: process.env.messagingSenderId,
   appId: process.env.appId
 
-  // apiKey: "AIzaSyBzJ3TahAq1tCjWxWor8FLMnHRJar3otvg",
-  // authDomain: "headstarter-pantry-43865.firebaseapp.com",
-  // projectId: "headstarter-pantry-43865",
-  // storageBucket: "headstarter-pantry-43865.appspot.com",
-  // messagingSenderId: "866410110508",
-  // appId: "1:866410110508:web:504e36dd52cacf97f03b89",
 };
 
 console.log('API Key bruh:', process.env.authDomain); // Add this line
